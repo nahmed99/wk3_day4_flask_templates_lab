4967c4c218b484f798d9301e4bc931ad3f411de3
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-19 15:28:08 +0000

    Added recurring events and css styles

--- app/models/event.py
+++ app/models/event.py
@@ -1,8 +1,9 @@
 class Event():
 
-    def __init__(self, date, event_name, guest_numbers, room_location, description):
+    def __init__(self, date, event_name, guest_numbers, room_location, description, recurring):
         self.date = date
         self.event_name = event_name
         self.guest_numbers = guest_numbers
         self.room_location = room_location
         self.description = description
+        self.recurring = recurring
--- app/models/event_list.py
+++ app/models/event_list.py
@@ -1,11 +1,11 @@
 from app.models.event import *
 
 
-event1 = Event('2020-12-01', "The Gathering", 25, 'Glasgow City', "Lot's of people gathering in one location")
-event2 = Event('2020-12-05', "The Walk", 13, 'West Central Scotland', "Some people out for a walk")
-event3 = Event('2020-12-07', "The Big Talk", 5, 'Dundee', "Somebody giving an important talk about something that they care about")
-event4 = Event('2020-12-09', "The Daily Yawn", 50, 'Webcast', "Democracy. Do people enjoy voting? Three-day conference")
-event5 = Event('2020-12-12', "The Walk", 25, 'West Central Scotland', "Some more people out for a walk")
+event1 = Event('2020-12-01', "The Gathering", 25, 'Glasgow City', "Lot's of people gathering in one location", False)
+event2 = Event('2020-12-05', "The Walk", 13, 'West Central Scotland', "Some people out for a walk", True)
+event3 = Event('2020-12-07', "The Big Talk", 5, 'Dundee', "Somebody giving an important talk about something that they care about", False)
+event4 = Event('2020-12-09', "The Daily Yawn", 50, 'Webcast', "Democracy. Do people enjoy voting? Three-day conference", False)
+event5 = Event('2020-12-12', "The Walk", 25, 'West Central Scotland', "Some more people out for a walk", True)
 
 events = [event1, event2, event3, event4, event5]
 
